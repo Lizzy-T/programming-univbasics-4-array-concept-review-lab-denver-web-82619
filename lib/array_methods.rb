cd65455214bacc4a5eda4f8afcb6f04458c8d5ae
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-12 21:47:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -31,4 +31,15 @@ end
 
 def find_min_value(array)
   # Add your solution here
+  i = 1
+  min = array[0]
+  while i < array.length do
+    if min < array[i]
+      min
+    else
+      min = array[i]
+    end 
+    i += 1
+  end
+  min
 end
